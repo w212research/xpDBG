c4d8b12de217690693f31ebaedf4209bc822736e
spv420 <unomilliono@gmail.com>
2022-02-20 18:14:43 -0500

    more logging fixes

--- xpdbg_software/src/logging.cc
+++ xpdbg_software/src/logging.cc
@@ -9,7 +9,7 @@ using namespace std;
 static log_level_t	current_log_level;
 char 				log_char[] = "vcewi";
 
-log_status_t xpdbg_log(log_level_t log_level, char* fmt, ...) {
+log_status_t xpdbg_log(log_level_t log_level, const char* fmt, ...) {
 	if (log_level < current_log_level) {
 		return LOG_UNIMPORTANT;
 	} else {
