563844244a04fa1a2e61ed6143eacf4e13b7d90f
spv420 <unomilliono@gmail.com>
2022-06-25 02:32:15 -0400

    yeet

--- xpdbg_software/wip_components/libxpdbg/src/Machine.cpp
+++ xpdbg_software/wip_components/libxpdbg/src/Machine.cpp
@@ -17,35 +17,12 @@
 
 #include <capstone/capstone.h>
 #include <unicorn/unicorn.h>
-#include <LIEF/LIEF.hpp>
 #include "libxpdbg.hpp"
+#include "Machine.hpp"
 #include <cstdio>
 
-#define BASE_ADDY 0x0
+using namespace libxpdbg;
 
-uc_engine* uc_global;
-
-int main(int argc, char* argv[]) {
-	uc_err err;
-
-	err = uc_open(UC_ARCH_ARM,
-				  UC_MODE_THUMB,
-				  &uc_global);
-
-	if (err) {
-		printf("Failed on uc_open() with error returned: %u (%s)\n",
-			   err,
-			   uc_strerror(err));
-		return -1;
-	}
-
-	uc_mem_map(uc_global, BASE_ADDY, 0x100000, UC_PROT_ALL);
-
-	uc_close(uc_global);
-
-	libxpdbg::Machine machine;
-
-	machine.hello();
-
-	return 0;
+void libxpdbg::Machine::hello() {
+	printf("Hello, world!\n");
 }
