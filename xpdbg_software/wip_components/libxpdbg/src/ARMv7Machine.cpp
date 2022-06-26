03ec6f7674ccfd8190bdcee53e3d3e900c3ca524
spv420 <unomilliono@gmail.com>
2022-06-26 01:00:57 -0400

    comments and shit

--- xpdbg_software/wip_components/libxpdbg/src/ARMv7Machine.cpp
+++ xpdbg_software/wip_components/libxpdbg/src/ARMv7Machine.cpp
@@ -51,10 +51,16 @@ ARMv7Machine::ARMv7Machine() {
 	uc_err err;
 	reg_t  reg;
 
+	/*
+	 *  open unicorn handle as thumb, that works for ARM code as well.
+	 */
 	err = uc_open(UC_ARCH_ARM,
 				  UC_MODE_THUMB,
 				  &this->uc);
 
+	/*
+	 *  create descriptions for all of the registers we intend to "publish"
+	 */
 	reg.reg_description = "r0";
 	reg.reg_name = "r0";
 	reg.reg_id = 0;
@@ -174,6 +180,9 @@ ARMv7Machine::ARMv7Machine() {
 	
 	this->registers.push_back(reg);
 
+	/*
+	 *  open capstone handles for ARM code and THUMB code.
+	 */
 	cs_open(CS_ARCH_ARM,
 			(cs_mode)(CS_MODE_ARM),
 			&handle);
@@ -188,11 +197,16 @@ ARMv7Machine::~ARMv7Machine() {
 }
 
 std::vector<reg_t> ARMv7Machine::get_registers() {
+	/*
+	 *  loop from 0 to the length of normal_regs, read the register, and set the
+	 *  value.
+	 */
 	for (int i = 0; i < sizeof(normal_regs) / sizeof(normal_regs[0]); i++) {
 		uint32_t val;
 		uc_reg_read(this->uc, normal_regs[i], &val);
 		this->registers[i].reg_value = val;
 	}
+
 	return this->registers;
 }
 
@@ -201,12 +215,21 @@ std::vector<mem_reg_t> ARMv7Machine::get_memory_regions() {
 	uc_mem_region		   *uc_style_memory_regions;
 	uint32_t				count;
 
+	/*
+	 *  get memory regions from unicorn, my good lord above
+	 *  thank you for this function
+	 */
 	uc_mem_regions(this->uc, &uc_style_memory_regions, &count);
 
 	for (int i = 0; i < count; i++) {
 		mem_reg_t region;
 		region.addr = uc_style_memory_regions[i].begin;
 		region.size = uc_style_memory_regions[i].end - region.addr;
+
+		/*
+		 *  technically UC_PROT shit is the same as XP_PROT shit tmk, but
+		 *  just in case, y'know?
+		 */
 		region.prot = 0;
 		region.prot |= (uc_style_memory_regions[i].perms & UC_PROT_READ) ? XP_PROT_READ : 0;
 		region.prot |= (uc_style_memory_regions[i].perms & UC_PROT_WRITE) ? XP_PROT_WRITE : 0;
@@ -222,10 +245,17 @@ bool ARMv7Machine::map_memory(mem_reg_t memory_region) {
 	uint32_t prot = 0;
 	uc_err	 err;
 
+	/*
+	 *  convert between UC_PROT and XP_PROT, which are technically the same, but
+	 *  just in case things change.
+	 */
 	prot |= (memory_region.prot & XP_PROT_READ) ? UC_PROT_READ : 0;
 	prot |= (memory_region.prot & XP_PROT_WRITE) ? UC_PROT_WRITE : 0;
 	prot |= (memory_region.prot & XP_PROT_EXEC) ? UC_PROT_EXEC : 0;
 
+	/*
+	 *  map the memory
+	 */
 	err = uc_mem_map(this->uc, memory_region.addr, memory_region.size, prot);
 
 	if (err) {
@@ -236,12 +266,21 @@ bool ARMv7Machine::map_memory(mem_reg_t memory_region) {
 	return ret;
 }
 
+/*
+ *  unused, i believe
+ */
 int ARMv7Machine::find_memory_region(uint64_t addr) {
 	std::vector<mem_reg_t> regions;
 	int					   index = 0;
 
+	/*
+	 *  get all of the mapped memory regions
+	 */
 	regions = this->get_memory_regions();
 
+	/*
+	 *  check if addr is inside the region's range
+	 */
 	for (mem_reg_t& i : regions) {
 		uint64_t start_addr = i.addr;
 		uint64_t end_addr = i.addr + i.size;
@@ -257,6 +296,9 @@ int ARMv7Machine::find_memory_region(uint64_t addr) {
 bool ARMv7Machine::unmap_memory(mem_reg_t memory_region) {
 	bool ret = true;
 
+	/*
+	 *  unmap the memory
+	 */
 	ret = (uc_mem_unmap(this->uc, memory_region.addr, memory_region.size) == UC_ERR_OK) ? true : false;
 
 	return ret;
@@ -289,6 +331,11 @@ bool ARMv7Machine::exec_code_addr(uint64_t addr, uint64_t size) {
 bool ARMv7Machine::exec_code_addr_ninsns(uint64_t addr, uint64_t num) {
 	bool ret = true;
 
+	/*
+	 *  0xffff....L is just the max value representable by a uint64_t,
+	 *  so this'll run just until num instructions is hit. (or you just run out
+	 *  of memory)
+	 */
 	ret = (uc_emu_start(uc, addr, 0xffffffffffffffffL, 0, num) == UC_ERR_OK) ? true : false;
 
 	return ret;
@@ -299,9 +346,16 @@ bool ARMv7Machine::exec_code_ninsns(uint64_t num) {
 	uint32_t cpsr;
 	uint32_t val;
 
+	/*
+	 *  read PC and CPSR
+	 */
 	uc_reg_read(uc, UC_ARM_REG_PC, &val);
 	uc_reg_read(uc, UC_ARM_REG_CPSR, &cpsr);
 
+	/*
+	 *  1 << 5 is the bit in CPSR denoting whther currently executing in THUMB
+	 *  mode. prolly should add an enum or define.
+	 */
 	if (cpsr & (1 << 5)) {
 		val |= 1;
 	}
@@ -318,6 +372,9 @@ bool ARMv7Machine::exec_code_step() {
 bool ARMv7Machine::set_register(reg_t reg) {
 	bool ret = false;
 
+	/*
+	 *  no buffer overflows here, sir!
+	 */
 	if (reg.reg_id <= sizeof(normal_regs) / sizeof(normal_regs[0])) {
 		ret = (uc_reg_write(uc, normal_regs[reg.reg_id], &reg.reg_value) == UC_ERR_OK) ? true : false;
 	} else {
@@ -334,6 +391,9 @@ std::vector<insn_t> ARMv7Machine::disassemble_memory(uint64_t addr, uint64_t siz
 	insn_t				 insn;
 	std::vector<insn_t>	 ret;
 
+	/*
+	 *  &~1 because |1 denotes thumb
+	 */
 	this->read_memory(addr & (~1), buf, size);
 
 	if (addr & 1) {
@@ -356,9 +416,11 @@ std::vector<insn_t> ARMv7Machine::disassemble_memory(uint64_t addr, uint64_t siz
 		insn.id = insns[i].id;
 		insn.address = insns[i].address;
 		insn.size = insns[i].size;
+		
 		memcpy(insn.bytes, insns[i].bytes, sizeof(insn.bytes));
 		memcpy(insn.mnemonic, insns[i].mnemonic, sizeof(insn.mnemonic));
 		memcpy(insn.op_str, insns[i].op_str, sizeof(insn.op_str));
+
 		ret.push_back(insn);
 	}
 
